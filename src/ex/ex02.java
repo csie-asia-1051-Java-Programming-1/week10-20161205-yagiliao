1e5b572c9213f8aa02549ba9cc6ecbcec5e3c45c
yagiliao <105021039@live.asia.edu.tw>
2016-12-05 15:39:16 +0800

    ex

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -2,14 +2,38 @@
 /*
  * Topic: 輸入所使用的度數，換算夏月及非夏月之電費金額。
  * Date: 2016/12/05
- * Author: 1050210XX 周永振老師
+ * Author: 105021039 廖野吉
  */
-
+import java.util.Scanner;
 public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		double sum=0,sum1=0;
+		if(n<=120){
+			sum=n*2.10;
+			sum1=n*2.10;
+		}else
+		if(n>120 && n<=330){
+			sum=(120*2.10)+(n-120)*3.02;
+			sum1=(120*2.10)+(n-120)*2.68;
+		}else
+		if(n>330 && n<=500){
+			sum=(120*2.10)+(210*3.02)+((n-330)*4.39);
+			sum1=(120*2.10)+(210*2.68)+((n-330)*3.61);
+		}else
+		if(n>500 && n<=700){
+			sum=(120*2.10)+(210*3.02)+(170*4.39)+((n-500)*4.97);
+			sum1=(120*2.10)+(210*2.68)+(170*3.61)+((n-500)*4.01);
+		}else
+		if(n>700){
+			sum=(120*2.10)+(210*3.02)+(170*4.39)+(200*4.97)+((n-700)*5.63);
+			sum1=(120*2.10)+(210*2.68)+(170*3.61)+(200*4.01)+((n-700)*4.50);
+		}
+		System.out.println(sum);
+		System.out.println(sum1);
 	}
 
 }
--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -2,14 +2,57 @@
 /*
  * Topic: 請設計 Person 類別 (姓名,性別,身高,體重)，使用者可以新增個人資料，每新增一筆就會列出目前的資料串有哪些人，而這些資料請用插入排序法依每一個人的 BMI 值由大到小排序。
  * Date: 2016/12/05
- * Author: 1050210XX 周永振老師
+ * Author: 105021039 廖野吉
  */
-
+import java.util.ArrayList;
+import java.util.Scanner;
 public class ex03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		ArrayList<Integer> dataList=new ArrayList<Integer>();
+		String str="r";
+		int i=0,val=0;
+		boolean flag=false;
+		while(!(str.equals("N"))||!(str.equals("N"))){
+			System.out.print("input a valum");
+			val=scn.nextInt();
+			if(dataList.size()==0){
+				dataList.add(val);
+			}else
+				if(dataList.size()==1){
+					if(dataList.get(0)>val){
+						dataList.add(val);
+					}else{
+						dataList.add(0,val);
+					}
+				}else{
+					flag=true;i=0;
+					while(flag==true && i<dataList.size()-1){
+						if(i==0 &&dataList.get(0)<val){
+							dataList.add(0,val);
+							flag=false;
+						}else{
+							if(dataList.get(i)>val &&dataList.get(i+1)<=val){
+								flag=false;
+								dataList.add(i+1,val);
+							}else{
+								i++;
+							}
+						}
+					}
+				}
+			showListData(dataList);
+			System.out.print("countine?");
+			str=scn.next();
+		}
 	}
-
+	private static void showListData(ArrayList<Integer> list) {
+		int size=list.size();
+		for(int i=0;i<size;i++){
+			System.out.print(list.get(i)+"/t");
+		}
+		System.out.println();
+	}
 }
